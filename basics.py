0a870a37426beef22b0b9d31010b76c913e87b1c
Jorge Saura <jorge.saura@live.com>
2022-08-31 16:12:07 +0200

    Added intesect

--- basics.py
+++ basics.py
@@ -2,15 +2,17 @@
 def list_contains_element(listOfElements:list, x:int):
     return x in listOfElements
 
-
-
 # Get missing number in [1..100]
 def get_missing_number_1_100(listOfNumbers:list):
 
     return list(set(range(1,101)) - set(listOfNumbers))[0]
 
-# find first duplicate number in a list
+# Find first duplicate number in a list
 def find_duplicate_number_in_integer_list(listOfIntegers:list):
 
     return next(x for x in listOfIntegers if listOfIntegers.count(x) >1)
 
+# Intersect two lists
+def intersect(list1:list, list2:list):
+
+    return [x for x in set(list1) if x in list2]
--- test_basics.py
+++ test_basics.py
@@ -3,7 +3,7 @@ import unittest
 import basics
 
 
-class TestBasicsOpe(unittest.TestCase):
+class TestBasics(unittest.TestCase):
 
         def test_list_contains_element(self):
             listOfElements = [1,2,3,4,5,6,8,9]
@@ -51,6 +51,30 @@ class TestBasicsOpe(unittest.TestCase):
 
             self.assertEqual(result,1)
 
+        def test_intesect_2_list(self):
+            list1 = [1,2,3,4]
+            list2 = [4,5,6]
+
+            result = basics.intersect(list1,list2)
+
+            self.assertEqual(result,[4])
+
+        def test_intesect_2_list_more_than_1_intem_intersected(self):
+            list1 = [1,2,3,4,5]
+            list2 = [4,5,6]
+
+            result = basics.intersect(list1,list2)
+
+            self.assertEqual(result,[4,5])
+
+
+        def test_intesect_2_list_with_no_intersect(self):
+            list1 = [1,2,3,4]
+            list2 = [5,6]
+
+            result = basics.intersect(list1,list2)
+
+            self.assertEqual(result,[])
 
         # def test_example(self):
         #     self.assertEqual('resultado_obtenido','resultado_esperado')
